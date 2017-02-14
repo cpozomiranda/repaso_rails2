bcb050f00ef36aa13a509fa485f42dc8855af041
Cesar Pozo <c.pozomiranda@gmail.com>
2017-02-14 02:20:48 -0300

    segundo commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def x
   end
+  def save_user
+  	unless params[:nombre].blank? or params[:correo].blank? or params[:edad].blank?
+
+  		User.create(name:params[:nombre], mail:params[:correo], age:params[:edad])
+  		redirect_to pages_x_path,
+  		notice:"Usuario ingresado correctamente"
+  	
+  	end
+  end
+
 end
